e4c1d03830bf32cfb7ea23fccb5d17a21570ff8d
mariamkg00 <mariamkhadijah00@gmail.com>
2023-08-18 14:27:18 -0700

    Initial commit

---
+++ R/area__of_rectangle.R
@@ -0,0 +1,12 @@
+#' Calculate the area of a given rectangle
+#'
+#' @param len Length of rectangle
+#' @param wid Width of rectangle
+#'
+#' @return Returns the calculated area.
+#' @export
+#'
+#' @examples
+area_of_rec <- function(len,wid){
+  len*wid
+}
